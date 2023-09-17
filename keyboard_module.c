66a007836adf8b5a8fadde2476ef22be7f855d95
Sarah Bonna <sbonna@bu.edu>
2023-09-17 12:15:46 -0400

    refactor: ensured flow works without polling keyboard

--- keyboard_module.c
+++ keyboard_module.c
@@ -9,6 +9,7 @@
 #include <asm/uaccess.h>
 #include <linux/tty.h>
 #include <linux/sched.h>
+#include <linux/interrupt.h>
 
 MODULE_LICENSE("GPL");
   
@@ -21,21 +22,24 @@ static struct file_operations pseudo_dev_proc_operations;
 
 static struct proc_dir_entry *proc_entry;
 
+static int KEYBOARD = 1;
+
 static int __init initialization_routine(void) {
   printk("<1> Loading module\n");
+  
+  // disable IRQ1
+  disable_irq(KEYBOARD);
   
   // function in kernel module
   pseudo_dev_proc_operations.ioctl = pseudo_device_ioctl; 
 
   /* Start create proc entry */
-  // create_proc_entry is a kernel function
   proc_entry = create_proc_entry("keyboard_ioctl_test", 0444, NULL);
   if(!proc_entry) {
     printk("<1> Error creating /proc entry.\n");
     return 1;
   }
 
-  //proc_entry->owner = THIS_MODULE; <-- This is now deprecated
   proc_entry->proc_fops = &pseudo_dev_proc_operations;
 
   return 0;
@@ -56,6 +60,8 @@ void my_printk(char *string) {
 
 static void __exit cleanup_routine(void) {
   printk("<1> Dumping module\n");
+  // enable IRQ1
+  enable_irq(KEYBOARD);
   remove_proc_entry("keyboard_ioctl_test", NULL);
 
   return;
@@ -73,18 +79,21 @@ static inline void outb( unsigned char uch, unsigned short usPort ) {
 }
 
 char my_getchar ( void ) {
-  char c;
+  //char c;
 
-  static char scancode[128] = "\0\e1234567890-=\177\tqwertyuiop[]\n\0asdfghjkl;'`\0\\zxcvbnm,./\0*\0 \0\0\0\0\0\0\0\0\0\0\0\0\000789-456+1230.\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
-
+  //static char scancode[128] = "\0\e1234567890-=\177\tqwertyuiop[]\n\0asdfghjkl;'`\0\\zxcvbnm,./\0*\0 \0\0\0\0\0\0\0\0\0\0\0\0\000789-456+1230.\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+  
+  printk(KERN_ALERT "In my_getchar\n");
   /* Poll keyboard status register at port 0x64 checking bit 0 to see if
    * output buffer is full. We continue to poll if the msb of port 0x60
    * (data port) is set, as this indicates out-of-band data or a release
    * keystroke
    */
-  while( !(inb( 0x64 ) & 0x1) || ( ( c = inb( 0x60 ) ) & 0x80 ) );
+  //while( !(inb( 0x64 ) & 0x1) || ( ( c = inb( 0x60 ) ) & 0x80 ) );
 
-  return scancode[ (int)c ];
+  //return scancode[ (int)c ];
+  
+  return 'a';
 }
 
 /***
@@ -92,32 +101,31 @@ char my_getchar ( void ) {
  */
 static int pseudo_device_ioctl(struct inode *inode, struct file *file,
 				unsigned int cmd, unsigned long arg) {
-					
-  int num_bytes_not_copied, keyboard_port = 0x60;
+		
+  int num_bytes_not_copied; 
+  //int keyboard_port = 0x60;
   char kioc = (char) 0;
   
-  switch (cmd) {
-    case KEYBOARD_IOCTL_TEST:
-      // Get the current scan code
-      kioc = my_getchar();
-      num_bytes_not_copied = copy_to_user((char*)arg, &kioc, sizeof(char));
-          
-      if (num_bytes_not_copied > 0) {
-        printk("All bytes not copied!\n");
-      } 
-      
-      break;
-    
-    default:
-      return -EINVAL;
-      break;
+  switch (cmd){
+  case KEYBOARD_IOCTL_TEST:
+	printk(KERN_ALERT "In pseudo device ioctl\n");
+	// Get the current scan code
+	kioc = my_getchar();
+	printk("KIOC: %c\n", kioc);
+	num_bytes_not_copied = copy_to_user((char*)arg, &kioc, sizeof(char));
+		   
+	if (num_bytes_not_copied > 0) {
+		printk("All bytes not copied!\n");
+	} 
+	break;
+  
+  default:
+	return -EINVAL;
+	break;
   }
   
   return 0;
 }
 
-/* insmod looks for module_init, calls the function that is specified 
- * rmmod looks for module_exit, calls the function that is specified
- * */
 module_init(initialization_routine); 
 module_exit(cleanup_routine); 
--- keyboard_test.c
+++ keyboard_test.c
@@ -8,45 +8,46 @@
 static int num_char = 200;
 
 int main () {  
-  char buffer[num_char], *read, *write, ch, value;
+  char buffer[num_char], *read, *write, value;
   char *out_of_buffer = &buffer[num_char];
   read = write = buffer;
   value = (char) 0;
   int fd = open ("/proc/keyboard_ioctl_test", O_RDONLY);
   int response;
-
-  if (fd < 0) {
-	printf("Something went wrong with file open...\n");
-	exit(-1);
-  }
-  
   printf("***HW1***\nPlease type some characters then press the Enter key.\n");
   
-  while (1) {
-	  response = ioctl (fd, KEYBOARD_IOCTL_TEST, &value);
-	  if (response == -1) {
-		printf("Something went wrong...\n");
-		exit(-1);
-	  }
+  while (value != '\n') {
 	  /* write the character */
-	  while (value != '\n') {
+	  printf("IOCTL call\n");
+	  response = ioctl (fd, KEYBOARD_IOCTL_TEST, &value);
+	  printf("Returned to user-level program\n");
+	  if (response == -1) {
+		  printf("Something went wrong...\n");
+		  exit(-1);
+	  }
+	  printf("Value received: %c\n", value);
+	  if (value != '\n') {
 		  *write++ = value;
 		  if (write == out_of_buffer) {
-			  write = buffer;
+			  //write = buffer;
+			  break;
 		  }
+		  //printf("Write: %c\n", write);
 	  }
-	  printf("You have typed:\n");
-	  /* read the character */
-	  while (read != write) {
-		  if (read == out_of_buffer) {
-			  read = buffer;
-		  }
-		  
-		  putchar(*read);
-		  read++;
-	  }
-	  putchar('\n');
   }
+  
+  printf("You have typed:\n");
+  /* read the character */
+  while (read != write) {
+	  if (read == out_of_buffer) {
+		  //read = buffer;
+		  break;
+	  }
+	  
+	  putchar(*read);
+	  read++;
+  }
+  putchar('\n');
 
   return 0;
 }
